b90f01566aebf2c5de9ced69ac4ab866d6446d0e
lulezimukaj <lulezim.ukaj@gmx.de>
2021-10-08 15:06:11 +0200

    features: lowercase + uppercase+ number + 10characters feature added

--- script.js
+++ script.js
@@ -20,6 +20,8 @@ const pw2 = document.getElementById("inputpw2");
 pw1.addEventListener("keyup", equalpasswords);
 pw2.addEventListener("keyup", equalpasswords);
 
+pw1.addEventListener("keyup", nopassword);
+
 function equalpasswords() {
   const pw1value = pw1.value;
   const pw2value = pw2.value;
@@ -30,13 +32,93 @@ function equalpasswords() {
     const yes = document.getElementById("equalyes");
     const no = document.getElementById("equalno");
     yes.classList.add("equalno");
-    yes.classList.remove("equalyes");
+    no.classList.remove("equalyes");
     return true;
   } else {
     const no = document.getElementById("equalno");
     const yes = document.getElementById("equalyes");
     no.classList.add("equalyes");
     yes.classList.remove("equalno");
+    return true;
+  }
+}
+
+pw1.addEventListener("keyup", lowercasepassword);
+
+function lowercasepassword() {
+  const pw1value = pw1.value;
+
+  if (pw1value.match(/[a-z]+/)) {
+    const yes = document.getElementById("loweryes");
+    const no = document.getElementById("lowerno");
+    yes.classList.remove("lowerno");
+    no.classList.add("loweryes");
+    return true;
+  } else {
+    const no = document.getElementById("lowerno");
+    const yes = document.getElementById("loweryes");
+    no.classList.remove("loweryes");
+    yes.classList.add("lowerno");
+    return true;
+  }
+}
+
+pw1.addEventListener("keyup", uppercasepassword);
+
+function uppercasepassword() {
+  const pw1value = pw1.value;
+
+  if (pw1value.match(/[A-Z]+/)) {
+    const yes = document.getElementById("upperyes");
+    const no = document.getElementById("upperno");
+    yes.classList.remove("upperno");
+    no.classList.add("upperyes");
+    return true;
+  } else {
+    const no = document.getElementById("upperno");
+    const yes = document.getElementById("upperyes");
+    no.classList.remove("upperyes");
+    yes.classList.add("upperno");
+    return true;
+  }
+}
+
+pw1.addEventListener("keyup", numberpassword);
+
+function numberpassword() {
+  const pw1value = pw1.value;
+
+  if (pw1value.match(/[0-9]+/)) {
+    const yes = document.getElementById("numberyes");
+    const no = document.getElementById("numberno");
+    yes.classList.remove("numberno");
+    no.classList.add("numberyes");
+    return true;
+  } else {
+    const no = document.getElementById("numberno");
+    const yes = document.getElementById("numberyes");
+    no.classList.remove("numberyes");
+    yes.classList.add("numberno");
+    return true;
+  }
+}
+
+pw1.addEventListener("keyup", characterpassword);
+
+function characterpassword() {
+  const pw1value = pw1.value;
+
+  if (pw1value.length > 10) {
+    const yes = document.getElementById("tenyes");
+    const no = document.getElementById("tenno");
+    yes.classList.remove("tenno");
+    no.classList.add("tenyes");
+    return true;
+  } else {
+    const no = document.getElementById("tenno");
+    const yes = document.getElementById("tenyes");
+    no.classList.remove("tenyes");
+    yes.classList.add("tenno");
     return true;
   }
 }
