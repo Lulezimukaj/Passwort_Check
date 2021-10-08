548b17190e0e434a1697c1eba815b80f519bed9c
lulezimukaj <lulezim.ukaj@gmx.de>
2021-10-08 11:03:11 +0200

    feature: Show Password feature added

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log("Passowort Check");
+
+const button = document.querySelector("#button");
+button.addEventListener("click", showpassword);
+
+function showpassword() {
+  const input = document.getElementById("#inputpw");
+  const input2 = document.getElementById("#inputpw2");
+  if (inputpw.type === "password" || inputpw2.type === "password") {
+    inputpw.type = "text";
+    inputpw2.type = "text";
+  } else {
+    inputpw.type = "password";
+    inputpw2.type = "password";
+  }
+}
