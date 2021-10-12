045b8acc3202a7b8ce7bc07b0918e1465210be70
lulezimukaj <lulezim.ukaj@gmx.de>
2021-10-12 10:41:30 +0200

    fix: refactoring of my code

--- script.js
+++ script.js
@@ -3,118 +3,120 @@ console.log("Passowort Check");
 const button = document.querySelector("#button");
 button.addEventListener("click", showpassword);
 
+const PasswordInput1 = document.getElementById("inputpw");
+const PasswordInput2 = document.getElementById("inputpw2");
+
 function showpassword() {
-  const input = document.getElementById("inputpw");
-  const input2 = document.getElementById("inputpw2");
-  if (inputpw.type === "password" || inputpw2.type === "password") {
-    inputpw.type = "text";
-    inputpw2.type = "text";
+  if (
+    PasswordInput1.type === "password" ||
+    PasswordInput2.type === "password"
+  ) {
+    PasswordInput1.type = "text";
+    PasswordInput2.type = "text";
   } else {
-    inputpw.type = "password";
-    inputpw2.type = "password";
+    PasswordInput1.type = "password";
+    PasswordInput2.type = "password";
   }
 }
 
-const pw1 = document.getElementById("inputpw");
-const pw2 = document.getElementById("inputpw2");
-
-pw1.addEventListener("keyup", equalpasswords);
-pw2.addEventListener("keyup", equalpasswords);
+PasswordInput1.addEventListener("keyup", equalpasswords);
+PasswordInput2.addEventListener("keyup", equalpasswords);
 
 function equalpasswords() {
-  const pw1value = pw1.value;
-  const pw2value = pw2.value;
+  const PasswordInput1value = PasswordInput1.value;
+  const PasswordInput2value = PasswordInput2.value;
+  const PasswordNotEqual = document.getElementById("equalno");
+  const PasswordEqual = document.getElementById("equalyes");
 
-  if (pw1value !== pw2value || pw1value == "") {
-    const yes = document.getElementById("equalyes");
-    const no = document.getElementById("equalno");
-    yes.classList.add("equalno");
-    no.classList.remove("equalyes");
-    return true;
+  if (
+    PasswordInput1value !== PasswordInput2value ||
+    PasswordInput1value == ""
+  ) {
+    PasswordEqual.classList.add("equalno");
+    PasswordNotEqual.classList.remove("equalyes");
   } else {
-    const no = document.getElementById("equalno");
-    const yes = document.getElementById("equalyes");
-    no.classList.add("equalyes");
-    yes.classList.remove("equalno");
+    PasswordNotEqual.classList.add("equalyes");
+    PasswordEqual.classList.remove("equalno");
   }
 }
 
-pw1.addEventListener("keyup", lowercasepassword);
-pw2.addEventListener("keyup", lowercasepassword);
+PasswordInput1.addEventListener("keyup", lowercasepassword);
+PasswordInput2.addEventListener("keyup", lowercasepassword);
+PasswordInput1.addEventListener("keyup", uppercasepassword);
+PasswordInput2.addEventListener("keyup", uppercasepassword);
+PasswordInput1.addEventListener("keyup", numberpassword);
+PasswordInput2.addEventListener("keyup", numberpassword);
+PasswordInput1.addEventListener("keyup", characterpassword);
+PasswordInput2.addEventListener("keyup", characterpassword);
 
 function lowercasepassword() {
-  const pw1value = pw1.value;
-  const pw2value = pw2.value;
+  const PasswordInput1value = PasswordInput1.value;
+  const PasswordInput2value = PasswordInput2.value;
+  const PasswordLowerCase = document.getElementById("loweryes");
+  const PasswordNotLowerCase = document.getElementById("lowerno");
 
-  if (pw1value === pw2value && pw1value.match(/[a-z]+/)) {
-    const yes = document.getElementById("loweryes");
-    const no = document.getElementById("lowerno");
-    yes.classList.remove("lowerno");
-    no.classList.add("loweryes");
+  if (
+    PasswordInput1value === PasswordInput2value &&
+    PasswordInput1value.match(/[a-z]+/)
+  ) {
+    PasswordLowerCase.classList.remove("lowerno");
+    PasswordNotLowerCase.classList.add("loweryes");
   } else {
-    const no = document.getElementById("lowerno");
-    const yes = document.getElementById("loweryes");
-    no.classList.remove("loweryes");
-    yes.classList.add("lowerno");
+    PasswordNotLowerCase.classList.remove("loweryes");
+    PasswordLowerCase.classList.add("lowerno");
   }
 }
-
-pw1.addEventListener("keyup", uppercasepassword);
-pw2.addEventListener("keyup", uppercasepassword);
 
 function uppercasepassword() {
-  const pw1value = pw1.value;
-  const pw2value = pw2.value;
+  const PasswordInput1value = PasswordInput1.value;
+  const PasswordInput2value = PasswordInput2.value;
+  const PasswordUpperCase = document.getElementById("upperyes");
+  const PasswordNotUpperCase = document.getElementById("upperno");
 
-  if (pw1value === pw2value && pw1value.match(/[A-Z]+/)) {
-    const yes = document.getElementById("upperyes");
-    const no = document.getElementById("upperno");
-    yes.classList.remove("upperno");
-    no.classList.add("upperyes");
+  if (
+    PasswordInput1value === PasswordInput2value &&
+    PasswordInput1value.match(/[A-Z]+/)
+  ) {
+    PasswordUpperCase.classList.remove("upperno");
+    PasswordNotUpperCase.classList.add("upperyes");
   } else {
-    const no = document.getElementById("upperno");
-    const yes = document.getElementById("upperyes");
-    no.classList.remove("upperyes");
-    yes.classList.add("upperno");
+    PasswordNotUpperCase.classList.remove("upperyes");
+    PasswordUpperCase.classList.add("upperno");
   }
 }
-
-pw1.addEventListener("keyup", numberpassword);
-pw2.addEventListener("keyup", numberpassword);
 
 function numberpassword() {
-  const pw1value = pw1.value;
-  const pw2value = pw2.value;
+  const PasswordInput1value = PasswordInput1.value;
+  const PasswordInput2value = PasswordInput2.value;
+  const PasswordNumber = document.getElementById("numberyes");
+  const PasswordNoNumber = document.getElementById("numberno");
 
-  if (pw1value === pw2value && pw1value.match(/[0-9]+/)) {
-    const yes = document.getElementById("numberyes");
-    const no = document.getElementById("numberno");
-    yes.classList.remove("numberno");
-    no.classList.add("numberyes");
+  if (
+    PasswordInput1value === PasswordInput2value &&
+    PasswordInput1value.match(/[0-9]+/)
+  ) {
+    PasswordNumber.classList.remove("numberno");
+    PasswordNoNumber.classList.add("numberyes");
   } else {
-    const no = document.getElementById("numberno");
-    const yes = document.getElementById("numberyes");
-    no.classList.remove("numberyes");
-    yes.classList.add("numberno");
+    PasswordNoNumber.classList.remove("numberyes");
+    PasswordNumber.classList.add("numberno");
   }
 }
 
-pw1.addEventListener("keyup", characterpassword);
-pw2.addEventListener("keyup", characterpassword);
-
 function characterpassword() {
-  const pw1value = pw1.value;
-  const pw2value = pw2.value;
+  const PasswordInput1value = PasswordInput1.value;
+  const PasswordInput2value = PasswordInput2.value;
+  const PasswordTenCharacter = document.getElementById("tenyes");
+  const PasswordNotTenCharacter = document.getElementById("tenno");
 
-  if (pw1value === pw2value && pw1value.length >= 10) {
-    const yes = document.getElementById("tenyes");
-    const no = document.getElementById("tenno");
-    yes.classList.remove("tenno");
-    no.classList.add("tenyes");
+  if (
+    PasswordInput1value === PasswordInput2value &&
+    PasswordInput1value.length >= 10
+  ) {
+    PasswordTenCharacter.classList.remove("tenno");
+    PasswordNotTenCharacter.classList.add("tenyes");
   } else {
-    const no = document.getElementById("tenno");
-    const yes = document.getElementById("tenyes");
-    no.classList.remove("tenyes");
-    yes.classList.add("tenno");
+    PasswordNotTenCharacter.classList.remove("tenyes");
+    PasswordTenCharacter.classList.add("tenno");
   }
 }
